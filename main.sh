72b3e3a5739a924d1277d8e6613c7ad8f447f142
adamklie <aklie@ucsd.edu>
2021-09-24 11:30:48 -0700

    Merged. Test Docker locally

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 cd /app/
 
-<<<<<<< HEAD
 # Main pipeline for taking individual level genotypes in plink bfile format and outputting predictions
 echo -e 'Running CAGI6-PRS Docker Script\n'
 
@@ -26,65 +25,3 @@ python3 ReleaseYourCAGIdFury.py --model_path data/test/MGB_10_all/Optuna_Study_T
 python3 ReleaseYourCAGIdFury.py --model_path data/test/MGB_10_all/Optuna_Study_Trial_20_Epoch_49.pt --config_path data/test/MGB_10_all/IBD_5e-4_Old_Test.yaml --out_path output/test/MGB_10_all/
 
 echo -e 'Script completed'
-=======
-echo 'Testing Docker'
-
-# Main pipeline for taking individual level genotypes in plink bfile format and outputting predictions
-
-### Step 1 - Generate raw files for for MGB - Tell plink which is minor ###
-
-# Usage:
-# $path_to_plink --bfile $path_to_bfiles --export A --out  MGB
-
-# Test
-./plink --bfile data/sample/MGB_100_1000 --export A --out output/test/MGB_100_1000
-
-# Actual
-# ./plink --bfile data/val/MGB_100_1000 --export A --out data/val/MGB_100_1000
-
-###
-
-### Step 2 - Generate z-scored feather files for each pheno ###
-
-# Usage:
-# python3 make-feather.py --raw $path_to_raw --stats $path_to_snp_statistics (maybe make this a mapping that can correct for things)
-
-# Test
-python3 make-feather-for-loader.py --raw data/test/MGB_100_1000.raw --stats data/sample/MGB_100_1000.stats.tsv --out output/test/MGB_100_1000
-
-# Actual
-# python3 make-feather-for-loader.py --raw data/val/MGB.raw --stats data/val/bca.stats.tsv --out data/val/bca
-# python3 make-feather-for-loader.py --raw data/val/MGB.raw --stats data/val/cad.stats.tsv --out data/val/cad
-# python3 make-feather-for-loader.py --raw data/val/MGB.raw --stats data/val/ibd.stats.tsv --out data/val/ibd
-# python3 make-feather-for-loader.py --raw data/val/MGB.raw --stats data/val/t2d.stats.tsv --out data/val/t2d
-
-###
-
-### Step 3 - Generate IDs and phenotype file  ###
-
-# Usage:
-# python3 make-pheno-for-loader.py --fam $path_to_fam --out
-
-# Test:
-python3 make-pheno-for-loader.py --fam data/sample/MGB_100_1000.fam --out output/test/MGB_100_1000
-
-# Actual
-# python3 make-pheno-for-loader.py --fam data/val/MGB.fam
-
-###
-
-### Step 4 - Dataload and make predictions using InSNPtion models ###
-
-# Usage
-# python3 --geno data/sample --config $path_to_config
-
-# Test
-
-# Actual
-# python3 --geno data/val/bca --config $path_to_config
-# python3 --geno data/val/cad --config $path_to_config
-# python3 --geno data/val/ibd --config $path_to_config
-# python3 --geno data/val/t2d --config $path_to_config
-
-###
->>>>>>> aa16d27adc0b9f7d636580d97e8bd33fa0f414fa
