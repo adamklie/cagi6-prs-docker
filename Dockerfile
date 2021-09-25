3e7f53b1c015249803123fa1ce26d683ff7af326
Adam Klie <aklie@eng.ucsd.edu>
2021-09-25 12:10:33 -0700

    Tested successful 4 model Docker locally, will organize and add README to repo + Docker instructions to Notion

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,18 @@
-FROM ubuntu:latest  
+# grab the latest ubuntu base image
+FROM ubuntu:latest
+
+# set an environmental variable that I'm not sure about yet
 ENV DEBIAN_FRONTEND=noninteractive
+
+# install Python and pip
 RUN apt-get update && apt-get install --no-install-recommends --no-install-suggests -y python3.6 python3-pip python3-setuptools python3-dev libcurl4-gnutls-dev libxml2-dev libssl-dev wget unzip
 RUN pip3 install --upgrade pip
 
+# set working dir
 ENV PYTHONPATH "${PYTHONPATH}:/app"
 WORKDIR /app
 
+# Add all the files locally to the Docker
 ADD . .
 
 # installing python libraries
@@ -15,7 +22,9 @@ RUN pip3 install --no-cache-dir -r python_requirements.txt
 RUN wget https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20210606.zip
 RUN unzip plink_linux_x86_64_20210606.zip
 
+# making dirs readable and writeable
 RUN chmod -R 777 /app/data/
 RUN chmod -R 777 /app/output/
 
-CMD ["bash", "/app/MGB_10_all.sh"]
+# run the script inside the docker
+CMD ["bash", "/app/main.sh"]
--- main.sh
+++ main.sh
@@ -4,28 +4,29 @@
 echo -e 'Running CAGI6-PRS Docker Script\n'
 
 ### Step 1 - Generate IDs and phenotype file
-python3 make-pheno-for-loader.py --fam ./data/test/MGB_10_all/MGB_10_all.fam --out ./data/test/MGB_10_all/MGB_10_all
+python3 make-pheno-for-loader.py --fam /data/test/MGB_10_all/MGB_10_all.fam --out /data/test/MGB_10_all/MGB_10_all
 echo -e "\n"
 
 ### Step 2 - Generate raw files for for MGB for each disease
-./plink --bfile ./data/test/MGB_10_all/MGB_10_all --extract ./data/test/MGB_10_all/bca.age.old.extract.txt --export A --out ./data/test/MGB_10_all/MGB_10_all.bca
-./plink --bfile ./data/test/MGB_10_all/MGB_10_all --extract ./data/test/MGB_10_all/cad.age.old.extract.txt --export A --out ./data/test/MGB_10_all/MGB_10_all.cad
-./plink --bfile ./data/test/MGB_10_all/MGB_10_all --extract ./data/test/MGB_10_all/ibd.age.old.extract.txt --export A --out ./data/test/MGB_10_all/MGB_10_all.ibd
-./plink --bfile ./data/test/MGB_10_all/MGB_10_all --extract ./data/test/MGB_10_all/t2d.age.old.extract.txt --export A --out ./data/test/MGB_10_all/MGB_10_all.t2d
-echo -e "\n"
+echo -e "Generating raw files\n------------"
+./plink --bfile /data/test/MGB_10_all/MGB_10_all --extract /data/test/MGB_10_all/bca.age.old.extract.txt --export A --out /data/test/MGB_10_all/MGB_10_all.bca
+./plink --bfile /data/test/MGB_10_all/MGB_10_all --extract /data/test/MGB_10_all/cad.age.old.extract.txt --export A --out /data/test/MGB_10_all/MGB_10_all.cad
+./plink --bfile /data/test/MGB_10_all/MGB_10_all --extract /data/test/MGB_10_all/ibd.age.old.extract.txt --export A --out /data/test/MGB_10_all/MGB_10_all.ibd
+./plink --bfile /data/test/MGB_10_all/MGB_10_all --extract /data/test/MGB_10_all/t2d.age.old.extract.txt --export A --out /data/test/MGB_10_all/MGB_10_all.t2d
+echo -e "------------\n"
 
 ### Step 3 - Generate z-scored feather files
-python3 make-feather-for-loader.py --raw ./data/test/MGB_10_all/MGB_10_all.bca.raw --summary ./data/test/MGB_10_all/bca.age.old.summary.tsv --out ./data/test/MGB_10_all/MGB_10_all.bca
-python3 make-feather-for-loader.py --raw ./data/test/MGB_10_all/MGB_10_all.cad.raw --summary ./data/test/MGB_10_all/cad.age.old.summary.tsv --out ./data/test/MGB_10_all/MGB_10_all.cad
-python3 make-feather-for-loader.py --raw ./data/test/MGB_10_all/MGB_10_all.ibd.raw --summary ./data/test/MGB_10_all/ibd.age.old.summary.tsv --out ./data/test/MGB_10_all/MGB_10_all.ibd
-python3 make-feather-for-loader.py --raw ./data/test/MGB_10_all/MGB_10_all.t2d.raw --summary ./data/test/MGB_10_all/t2d.age.old.summary.tsv --out ./data/test/MGB_10_all/MGB_10_all.t2d
+python3 make-feather-for-loader.py --raw /data/test/MGB_10_all/MGB_10_all.bca.raw --summary /data/test/MGB_10_all/bca.age.old.summary.tsv --out /data/test/MGB_10_all/MGB_10_all.bca
+python3 make-feather-for-loader.py --raw /data/test/MGB_10_all/MGB_10_all.cad.raw --summary /data/test/MGB_10_all/cad.age.old.summary.tsv --out /data/test/MGB_10_all/MGB_10_all.cad
+python3 make-feather-for-loader.py --raw /data/test/MGB_10_all/MGB_10_all.ibd.raw --summary /data/test/MGB_10_all/ibd.age.old.summary.tsv --out /data/test/MGB_10_all/MGB_10_all.ibd
+python3 make-feather-for-loader.py --raw /data/test/MGB_10_all/MGB_10_all.t2d.raw --summary /data/test/MGB_10_all/t2d.age.old.summary.tsv --out /data/test/MGB_10_all/MGB_10_all.t2d
 echo -e "\n"
 
 ### Step 4 - Dataload and make predictions
-python3 ReleaseYourCAGIdFury.py --model_path ./data/test/MGB_10_all/Optuna_Study_Trial_11_Epoch_38.pt --config_path ./data/test/MGB_10_all/BCA_5e-4_Old_Test.yaml --out_path ./output/test/MGB_10_all/
-python3 ReleaseYourCAGIdFury.py --model_path ./data/test/MGB_10_all/Optuna_Study_Trial_45_Epoch_24.pt --config_path ./data/test/MGB_10_all/CAD_5e-4_Old_Test.yaml --out_path ./output/test/MGB_10_all/
-python3 ReleaseYourCAGIdFury.py --model_path ./data/test/MGB_10_all/Optuna_Study_Trial_20_Epoch_49.pt --config_path ./data/test/MGB_10_all/IBD_5e-4_Old_Test.yaml --out_path ./output/test/MGB_10_all/
-python3 ReleaseYourCAGIdFury.py --model_path ./data/test/MGB_10_all/Optuna_Study_Trial_15_Epoch_21.pt --config_path ./data/test/MGB_10_all/T2D_5e-4_Old_Test.yaml --out_path ./output/test/MGB_10_all/
-python PENGUINS.py --inter_milan ./output/test/MGB_10_all/ --out_path ./output/test/MGB_10_all/
+python3 ReleaseYourCAGIdFury.py --model_path /data/test/MGB_10_all/Optuna_Study_Trial_11_Epoch_38.pt --config_path /data/test/MGB_10_all/BCA_5e-4_Old_Test.yaml --out_path /output/test/MGB_10_all/
+python3 ReleaseYourCAGIdFury.py --model_path /data/test/MGB_10_all/Optuna_Study_Trial_45_Epoch_24.pt --config_path /data/test/MGB_10_all/CAD_5e-4_Old_Test.yaml --out_path /output/test/MGB_10_all/
+python3 ReleaseYourCAGIdFury.py --model_path /data/test/MGB_10_all/Optuna_Study_Trial_20_Epoch_49.pt --config_path /data/test/MGB_10_all/IBD_5e-4_Old_Test.yaml --out_path /output/test/MGB_10_all/
+python3 ReleaseYourCAGIdFury.py --model_path /data/test/MGB_10_all/Optuna_Study_Trial_15_Epoch_21.pt --config_path /data/test/MGB_10_all/T2D_5e-4_Old_Test.yaml --out_path /output/test/MGB_10_all/
+python3 PENGUINS.py --inter_milan /output/test/MGB_10_all/ --out_path /output/test/MGB_10_all/
 
 echo -e 'Script completed'
